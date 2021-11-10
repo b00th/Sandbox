df871ac4331b3da0b466db8a169dbf3fa575a882
Mathias Dissing-Hansen <madi37793@edu.ucl.dk>
2021-11-10 10:59:32 +0100

    fuck alt

--- Program.cs
+++ Program.cs
@@ -18,10 +18,14 @@ namespace Sandbox
         }
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             bil Ford = new bil("Mustang", "røøød", 1984); ;
 
-            Dissing Mathias = new Dissing("God", "hvid", "1991");
             
+=======
+            bil Ford = new bil("Mustang", "den er blå igen", 1984); ;
+>>>>>>> 6f4a6100c4f058a47561f93f07f65f8a8cacc156
+            Dissing Mathias = new Dissing("God", "hvid", "1991");
         }
     }
 }
