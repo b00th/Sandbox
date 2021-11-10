b8dbd1a2a76b661b8cf9eed769ba0a8e288ca369
Mathias Dissing-Hansen <madi37793@edu.ucl.dk>
2021-11-10 11:34:32 +0100

    Tilføjet Axel class

---
+++ Axel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Axel
+    {
+        public string Model;
+        public string Farve;
+        public string Årgang;
+        public Axel(string ModelNavn, string FarveKode, string Årgang)
+        {
+            this.Model = ModelNavn;
+            this.Farve = FarveKode;
+            this.Årgang = Årgang;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace Sandbox
             bil Ford = new bil("Mustang", "røøød", 1984); ;
             bil BMW = new bil("M4", "Flaske Grøn", 2021);
             bil Ford1 = new bil("Mustang", "den er blå igen.....", 1984); ;
-
+            Axel Lund = new Axel("Høj", "Medium hvid", "2000");
             Dissing Mathias = new Dissing("God", "hvid", "1991");
         }
     }
